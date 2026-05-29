cb90e9ef55fc5d6c5a31f5fb403f61cb856505fb
Алексей <ostrakin@gmail.com>
2026-05-29 10:34:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
+from dotenv import load_dotenv
+load_dotenv()  # Принудительно загружает переменные из .env
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
